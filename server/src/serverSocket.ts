6151c7c5d2e124d25fa24fd4745f8dd3f47753ce
golubeva marina <golubeva.webmaster@gmail.com>
2022-01-22 16:52:44 +0300

    feat: new try

--- server/src/serverSocket.ts
+++ server/src/serverSocket.ts
@@ -1,4 +1,4 @@
-import { connection, IUtf8Message, request } from "websocket";
+import { connection, IClientConfig, IUtf8Message, request } from "websocket";
 import { Server } from "http";
 import { IConnection, IServerRequestMessage, IServerResponseMessage } from "./serverInterfaces";
 import { timeStamp } from "console";
@@ -10,64 +10,99 @@ function isUtf8(message): message is IUtf8Message {
   return (message.type === 'utf8')
 }
 
+// class SocketUser {
+//   connection: connection;
+//   onSendName: (name: string) => void;
+//   constructor(connection: connection) {
+//     this.connection = connection;
+//     connection.on('message', data => {
+//       if (isUtf8(data)) {
+//         const message = data;
+//         const requestMessage: IServerRequestMessage = JSON.parse(message.utf8Data);
+//         if (requestMessage.type === 'sendName') {
+//           this.onSendName?.(requestMessage.content)
+//         }
+//       }
+//     })
+//   }
+// }
+
+// class GameUser {
+//   connection: connection;
+//   onSendNewBuild: (buildData: any) => void;
+//   onSendUpdateObject: (buildData: any) => void;
+//   constructor(connection: connection) {
+//     this.connection = connection;
+//     connection.on('message', data => {
+//       if (isUtf8(data)) {
+//         const message = data;
+//         const requestMessage: IServerRequestMessage = JSON.parse(message.utf8Data);
+//         if (requestMessage.type === 'sendNewBuild') {
+//           const buildData: any = JSON.parse(requestMessage.content);  //указать тип
+//           this.onSendNewBuild(buildData);
+//           // this.connections.forEach(connect => {
+//           //   connect.connection.sendUTF(JSON.stringify({ type: 'addNewBuild', content: JSON.stringify(requestMessage.content) }))
+//           // })
+//         }
+//         if (requestMessage.type === 'sendUpdateObject') {
+//           const buildData: any = JSON.parse(requestMessage.content);  //указать тип
+//           this.onSendUpdateObject(buildData);
+//           // this.connections.forEach(connect => {
+//           //   connect.connection.sendUTF(JSON.stringify({ type: 'getUpdateObject', content: JSON.stringify(requestMessage.content) }))
+//           // })
+//         }
+//       }
+//     })
+
+//   }
+// }
+
 class SocketUser {
-  connection: connection;
   onSendName: (name: string) => void;
-  constructor(connection: connection) {
-    this.connection = connection;
-    connection.on('message', data => {
-      if (isUtf8(data)) {
-        const message = data;
-        const requestMessage: IServerRequestMessage = JSON.parse(message.utf8Data);
-        if (requestMessage.type === 'sendName') {
-          this.onSendName?.(requestMessage.content)
-        }
-      }
-    })
+  constructor() {}
+  handleMessage(message: IServerRequestMessage){
+    if (message.type === 'sendName') {
+      this.onSendName?.(message.content)
+    }
   }
 }
 
 class GameUser {
-  connection: connection;
   onSendNewBuild: (buildData: any) => void;
   onSendUpdateObject: (buildData: any) => void;
-  constructor(connection: connection) {
-    this.connection = connection;
-    connection.on('message', data => {
-      if (isUtf8(data)) {
-        const message = data;
-        const requestMessage: IServerRequestMessage = JSON.parse(message.utf8Data);
-        if (requestMessage.type === 'sendNewBuild') {
-          const buildData: any = JSON.parse(requestMessage.content);  //указать тип
-          this.onSendNewBuild(buildData);
-          // this.connections.forEach(connect => {
-          //   connect.connection.sendUTF(JSON.stringify({ type: 'addNewBuild', content: JSON.stringify(requestMessage.content) }))
-          // })
-        }
-        if (requestMessage.type === 'sendUpdateObject') {
-          const buildData: any = JSON.parse(requestMessage.content);  //указать тип
-          this.onSendUpdateObject(buildData);
-          // this.connections.forEach(connect => {
-          //   connect.connection.sendUTF(JSON.stringify({ type: 'getUpdateObject', content: JSON.stringify(requestMessage.content) }))
-          // })
-        }
-      }
-    })
-
+  constructor() {}
+  handleMessage(message: IServerRequestMessage){
+    if (message.type === 'sendNewBuild') {
+      const buildData: any = JSON.parse(message.content);  //указать тип
+      this.onSendNewBuild(buildData);
+    }
+    if (message.type === 'sendUpdateObject') {
+      const buildData: any = JSON.parse(message.content);  //указать тип
+      this.onSendUpdateObject(buildData);    
+    }
   }
 }
 
+export class Game{
+  public players: Array<GameUser>;
+  constructor(){}
+  addPlayer(player: GameUser){ // добавление игрока
+    this.players.push(player)
+  }
+  sendMessage(message: IServerResponseMessage){
 
+  }
+}
 export class ServerSocket {
 
   public connections: Array<IConnection> = []
   //private name: string;
-  users: Array<SocketUser> = [];
+  users: Map<connection, SocketUser>;
 
   constructor(server: Server) {
     this.connections = []
     //this.name = null;
-    this.users = [];
+    this.users = new Map();
     const wsServer = new websocket.server({
       httpServer: server,
     });
@@ -75,7 +110,7 @@ export class ServerSocket {
     wsServer.on('request', (request: request) => {
       console.log("^^^")
       const connection = request.accept(undefined, request.origin);
-      const user = new SocketUser(connection);
+      const user = new SocketUser();
       user.onSendName = (name) => {
         this.connections.forEach(connect => {
           if (connect.name !== name) {
@@ -87,8 +122,8 @@ export class ServerSocket {
           }
         })
       }
-      this.users.push(user);
-      const player = new GameUser(connection);
+      this.users.set(connection, user);
+      const player = new GameUser();
       player.onSendNewBuild = (buildData) => {
         this.connections.forEach(connect => {
           connect.connection.sendUTF(JSON.stringify({ type: 'addNewBuild', content: JSON.stringify(buildData) }))
@@ -99,12 +134,14 @@ export class ServerSocket {
           connect.connection.sendUTF(JSON.stringify({ type: 'getUpdateObject', content: JSON.stringify(buildData) }))
         })
       }
-      connection.on('message', (_message) => {
+      connection.on('message', (_message) => { // пришло сообщение
         if (isUtf8(_message)) {
           const message = _message;
           const requestMessage: IServerRequestMessage = JSON.parse(
             message.utf8Data
           );
+          this.users.get(connection)?.handleMessage(requestMessage); // распарсеное сообщ помещаем в юзера
+
           // if (requestMessage.type === 'message') {
           //   const responseStatus: IServerResponseMessage = {
           //     type: 'message-status',
