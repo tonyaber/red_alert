5999da05175f0909500596afcc6113222773e69f
Inikon <ZhanZS@mail.ru>
2022-02-06 22:02:50 +0300

    delete old code from ultratiling

--- client/src/game3/client/canvasView2.ts
+++ client/src/game3/client/canvasView2.ts
@@ -1,32 +1,18 @@
 import Control from "../../common/control";
 import { IGameObjectData } from "./dto";
-import { InteractiveObject,interactiveList } from "./interactiveObject";
-import { InteractiveList } from "./interactiveList";
 import { Vector } from '../../common/vector';
-import Signal from "../../common/signal";
-
-import {TilingLayer} from "./ultratiling/tileLayer";
-import { GameObject } from "./ultratiling/gameObject";
 import { Camera } from "./ultratiling/camera";
 import { RenderTicker } from './ultratiling/renderTicker';
-import { GameDebugInfoView } from './ultratiling/gameDebugInfoView';
 import { GameMainRender } from './ultratiling/gameMainRenderer';
 
 export class Canvas extends Control{
-  //interactiveList: InteractiveList;
-
   onGameMove: () => void;
   onClick: (position: Vector) => void;
   onObjectClick: (id: string, name: string) => void;
   canvas: Control<HTMLCanvasElement>;
   ctx: CanvasRenderingContext2D;
-  //hoveredObjects: InteractiveObject = null;
-
-  //camera = new Camera();
   ticker = new RenderTicker();
   renderer:GameMainRender;
-  //objects: Array<GameObject> = [];
-
 
   constructor(parentNode: HTMLElement, res:Record<string, HTMLImageElement>) {
     super(parentNode);
@@ -34,23 +20,11 @@ export class Canvas extends Control{
     this.canvas.node.width = 800;
     this.canvas.node.height = 600;
     this.ctx = this.canvas.node.getContext('2d');
-    //this.interactiveList = interactiveList;
 
     this.canvas.node.onmousemove = (e)=>{
-      //this.interactiveList.handleMove(new Vector(e.offsetX, e.offsetY), new Vector(e.offsetX, e.offsetY));
       this.renderer.setCameraPosition(new Vector(e.offsetX *20.5 -209, e.offsetY*20.5 -209));
       this.renderer.processMove(new Vector(e.offsetX, e.offsetY));
-      //this.renderer.camera.position = new Vector(e.offsetX -100, e.offsetY -100);
-      //this.renderer.tilingLayer.updateCamera(this.renderer.camera.position, this.renderer.camera.getTileSize());
     }
-    
-    
-    /*this.interactiveList.onChangeHovered = (lastTarget:InteractiveObject, currentTarget:InteractiveObject) => {
-      this.hoveredObjects = currentTarget;
-    }*/
-
- 
-    //this.interactiveList.add(obj);
 
     this.canvas.node.onclick = (e: MouseEvent) => {
       this.renderer.camera.scale = this.renderer.camera.scale - 0.2;
@@ -75,15 +49,6 @@ export class Canvas extends Control{
       this.render(this.ctx, delta);
     });
     this.ticker.startRender();
-    
-    /*for (let i =0; i<1000; i++){
-      //const obj = new GameObject(this.renderer.tilingLayer, res, new Vector(0, 0));
-      const obj = new GameObject(this.renderer.tilingLayer, res, new Vector(Math.floor(Math.random()*196), Math.floor(Math.random()*196)));
-      this.objects.push(obj);
-    }*/
-    //this.renderer.setCameraPosition(new Vector( -209, -209));
-    //this.renderer.tilingLayer.updateCamera(this.renderer.camera.position, this.renderer.camera.getTileSize());
-    
   }
 
   updateObject(data:IGameObjectData){
@@ -95,7 +60,7 @@ export class Canvas extends Control{
   }
 
   addObject(data: IGameObjectData) {
-    const interactiveObject = new InteractiveObject(data);
+    //const interactiveObject = new InteractiveObject(data);
   }
 
   render(ctx: CanvasRenderingContext2D, delta: number) {
--- client/src/game3/client/ultratiling/tileLayer.ts
+++ client/src/game3/client/ultratiling/tileLayer.ts
@@ -1,15 +1,12 @@
 import { Vector } from "../../../common/vector";
 import { copier } from "./copier";
-
+import { mod } from "./mod";
 export class TilingLayer{
   ctx: CanvasRenderingContext2D;
   canvas: HTMLCanvasElement;
   map: Array<Array<number>>;
   registred: Array<CanvasImageSource> = [];
   tileSize: number;
-  //viewWidth: number = 700;
-  //viewHeight: number = 500;
-  //lastViewPort: {top:number, left:number, width:number, height:number};
   lastCamera: Vector;
   ctx1: CanvasRenderingContext2D;
   canvas1: any;
@@ -29,10 +26,9 @@ export class TilingLayer{
     this.canvas1.height = 600;//height*tileSize;
     this.ctx1 = this.canvas1.getContext('2d');
 
-    let newMap:Array<Array<number>> = new Array(width).fill(0).map(it=> new Array(width).fill(0));
+    let newMap:Array<Array<number>> = new Array(height).fill(0).map(it=> new Array(width).fill(0));
     this.map = newMap;
-    //this.ctx.fillRect(0,0,1,1);
-    //this.update(newMap);
+
     this.updateCamera(new Vector(0, 0), tileSize);
   }
 
@@ -42,23 +38,12 @@ export class TilingLayer{
       this.lastCamera = camera.clone();
     }
     this.updataCache(camera, tileSize);
-    const mod = (arg: number, m: number)=>{
-      if (arg>=0){
-        return arg % m;
-      } else {
-        if (arg % m == 0 ){
-          return 0
-        }
-        return arg % m + m;
-      }
-    }
 
     this.ctx1.clearRect(0, 0, this.canvas1.width, this.canvas1.height);
     this.ctx1.drawImage(this.canvas, 
-     /* this.lastCacheCamera.x * tileSize +*/ -mod(camera.x, tileSize)*1, 
-     /* this.lastCacheCamera.y * tileSize +*/ -mod(camera.y, tileSize)*1
+      -mod(camera.x, tileSize)*1, 
+      -mod(camera.y, tileSize)*1
     );
-    //console.log(camera.x, mod(camera.x, tileSize));
   }
 
   updateCacheTile(camera:Vector, tileX:number, tileY:number, value:number){
@@ -100,25 +85,25 @@ export class TilingLayer{
     const viewTileHeight = Math.floor(this.canvas.height / tileSize);
     const viewTileWidth =  Math.floor(this.canvas.width / tileSize);
     return new Vector(
-      tilingCamera.x + viewTileWidth-1,
-      tilingCamera.y + viewTileHeight-1
+      tilingCamera.x + viewTileWidth - 1,
+      tilingCamera.y + viewTileHeight - 1
     );
   }
 
   updateScreen(tilingCamera:Vector, tileSize:number){
     const lastTile = this.getLastVisibleTile(tilingCamera, tileSize);
-    for (let i = tilingCamera.y; i<= lastTile.y/*this.map.length-1*/; i++){
-      for (let j = tilingCamera.x; j<= lastTile.x/* this.map[0].length-1*/; j++){
+    for (let i = tilingCamera.y; i <= lastTile.y; i++){
+      for (let j = tilingCamera.x; j <= lastTile.x; j++){
         this.renderTile(tilingCamera, j, i);
       }
     }
   }
 
   updataCache(camera:Vector, tileSize:number){
-    const tileCamera = new Vector(
+    const tileCamera = this.getTileCamera(camera, tileSize);/*new Vector(
       Math.floor(camera.x / tileSize),
       Math.floor(camera.y / tileSize)
-    );
+    );*/
     if (this.tileSize != tileSize){
       this.tileSize = tileSize;
       this.updateScreen(tileCamera, tileSize);
@@ -126,7 +111,7 @@ export class TilingLayer{
       return;
     }
     const inc = this.lastCacheCamera.clone().sub(tileCamera);
-    //console.log(inc);
+
     if ((inc.abs()==0)){
       return;
     }
@@ -135,152 +120,62 @@ export class TilingLayer{
     const mapWidth = this.map[0].length;
     const viewTileHeight = Math.floor(this.canvas.height / tileSize);
     const viewTileWidth =  Math.floor(this.canvas.width / tileSize);
-    const renderTileHeight = Math.min(mapHeight, viewTileHeight); //max for all corners
-    const renderTileWidth = Math.min(mapWidth, viewTileWidth); //max for all corners
+    const renderTileHeight = viewTileHeight;//Math.min(mapHeight, viewTileHeight); //max for all corners
+    const renderTileWidth = viewTileWidth;//Math.min(mapWidth, viewTileWidth); //max for all corners
 
     copier.clearRect(0, 0, copier.canvas.width, copier.canvas.height);
     copier.drawImage(this.canvas, 0, 0);
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.ctx.drawImage(copier.canvas, inc.x * tileSize*1, inc.y * tileSize*1);
 
-    /*const renderTile = (renderTileY: number, renderTileX:number, renderX:number, renderY:number) => {
-      const graphic = this.registred[this.map[renderTileY] && this.map[renderTileY][renderTileX]];
-      this.ctx.clearRect(renderX, renderY, this.tileSize, this.tileSize);
-      if (graphic){
-        this.ctx.drawImage(graphic, renderX, renderY, this.tileSize, this.tileSize);
-       // this.ctx.fillText(`${renderTileX}/${renderTileY}`, renderX, renderY + 10);
-      }  
-    }*/
-
-    const _renderTile = (tilingCamera:Vector, tileSize:number, tileX:number, tileY:number)=>{
-      //console.log(tilingCamera, tileX, tileY);
-      //renderTile(tileX, tileY, (-tilingCamera.x + tileX)*tileSize, (-tilingCamera.y + tileY) * tileSize);
+    const _renderTile = (tileX:number, tileY:number)=>{
       this.renderTile(tileCamera, tileX, tileY);
     }
 
-    //this.map[20][20] = Math.floor(Math.random()*2);
-    //_renderTile(tileCamera, this.tileSize, 20, 20);
-
-    const renderCol = (colTile: number, colPos:number)=>{
+    const renderCol = (colTile: number)=>{
       for (let j = 0; j< renderTileHeight; j++){
-        //renderTile(colTile, tileCamera.y + j, colPos, j* tileSize); 
-        _renderTile(tileCamera, this.tileSize, colTile, tileCamera.y +j); 
+        _renderTile(colTile, tileCamera.y +j); 
       }
     }
 
-    const renderRow = (rowTile: number, rowPos:number)=>{
+    const renderRow = (rowTile: number)=>{
       for (let j = 0; j< renderTileWidth; j++){
-        //renderTile(tileCamera.x + j, rowTile, j* tileSize, rowPos);
-        _renderTile(tileCamera, this.tileSize, tileCamera.x + j, rowTile);    
+        _renderTile(tileCamera.x + j, rowTile);    
       }
     }
 
-    //console.log(viewTileWidth - (mapWidth - tileCamera.x), viewTileHeight - (mapHeight - tileCamera.y) );
-    //console.log(viewTileWidth, viewTileHeight, tileCamera.x, tileCamera.y)
+    const lastTile = this.getLastVisibleTile(tileCamera, tileSize);
+
     if (inc.x >= 0){
       for (let i = 0; i< inc.x; i++){
-        renderCol(tileCamera.x + i, i * tileSize);
+        renderCol(tileCamera.x + i);
       }
     } else {
       for (let i = 0; i< -inc.x; i++){
-        renderCol(tileCamera.x+(-i + viewTileWidth-1), (-i + viewTileWidth-1) * tileSize);
+        renderCol(lastTile.x - i/*tileCamera.x+(-i + viewTileWidth-1)*/);
       }
     }
 
     if (inc.y >= 0){
       for (let i = 0; i< inc.y; i++){
-        renderRow(tileCamera.y + i, i * tileSize);
+        renderRow(tileCamera.y + i);
       }
     } else {
       for (let i = 0; i< -inc.y; i++){
-        renderRow(tileCamera.y +(-i + viewTileHeight-1), (-i + viewTileHeight-1) * tileSize);
+        renderRow(lastTile.y - i/*tileCamera.y +(-i + viewTileHeight-1)*/);
       }
     }
-    /*for (let i = Math.min(0, inc.x); i< Math.max(0, inc.x); i++){
-      renderCol(tileCamera.x, i * tileSize);
-      renderCol(tileCamera.x + viewTileWidth, (i + viewTileWidth) * tileSize);
-    }*/
-
-    /*for (let i = Math.min(0, inc.y); i< Math.max(0, inc.y); i++){
-      renderRow(tileCamera.y, i * tileSize);
-      renderRow(tileCamera.y + viewTileHeight, (i + viewTileHeight) * tileSize);
-    }*/
-    /*for (let i = 0; i< inc.y; i++){
-      for (let j = 0; j< renderTileWidth; j++){
-        const renderTileX = tileCamera.x + j;
-        const renderTileY = tileCamera.y + i;
-        const renderX = j * tileSize;
-        const renderY = i * tileSize;
-        
-      }  
-    }*/
-    /*for (let i = 0; i< renderTileHeight; i++){
-      for (let j = 0; j< renderTileWidth; j++){
-        const renderTileX = tileCamera.x + j;
-        const renderTileY = tileCamera.y + i;
-        const renderX = j * tileSize;
-        const renderY = i * tileSize;
-        //console.log(renderTileY, renderTileX)
-
-        const graphic = this.registred[this.map[renderTileY] && this.map[renderTileY][renderTileX]];
-        this.ctx.clearRect(renderX, renderY, this.tileSize, this.tileSize);
-        if (graphic){
-          this.ctx.drawImage(graphic, renderX, renderY, this.tileSize, this.tileSize);
-          this.ctx.fillText(`${renderTileX}/${renderTileY}`, renderX, renderY);
-        }
-      }
-    }*/
-    /*if (inc.abs()){
-      this.map.forEach((it,i)=>it.forEach((jt, j)=>{
-        const graphic = this.registred[this.map[i][j]];
-        this.ctx.clearRect((j - tileCamera.x)*this.tileSize, (i - tileCamera.y)*this.tileSize, this.tileSize, this.tileSize);
-        if (graphic){
-          this.ctx.drawImage(graphic, (j - tileCamera.x)*this.tileSize, (i - tileCamera.y)*this.tileSize , this.tileSize, this.tileSize);
-          this.ctx.fillText(`${j}/${i}`, (j- tileCamera.x)*this.tileSize  , (i - tileCamera.y)*this.tileSize );
-        }
-      }))*/
-      this.lastCacheCamera = tileCamera.clone();
     
+    this.lastCacheCamera = tileCamera.clone();
   }
-
-  /*fullUpdate(tileSize:number){
-    this.tileSize = tileSize;
-    this.map.forEach((it,i)=>it.forEach((jt, j)=>{
-      const graphic = this.registred[this.map[i][j]];
-      this.ctx.clearRect(j*this.tileSize, i*this.tileSize, this.tileSize, this.tileSize);
-      if (graphic){
-        this.ctx.drawImage(graphic, j*this.tileSize, i*this.tileSize, this.tileSize, this.tileSize);
-        this.ctx.fillText(`${j}/${i}`, j*this.tileSize, i*this.tileSize +10);
-      }
-    }))  
-  }*/
 
   update(camera:Vector, map:Array<Array<number>>){
     this.camera = camera;
     map.forEach((it,i)=>it.forEach((jt, j)=>{
       if (this.map[i][j] != map[i][j]){
-        /*this.map[i][j] = map[i][j];
-        const graphic = this.registred[map[i][j]];
-        this.ctx.clearRect(j*this.tileSize, i*this.tileSize, this.tileSize, this.tileSize);
-        if (graphic){
-          this.ctx.drawImage(graphic, j*this.tileSize, i*this.tileSize, this.tileSize, this.tileSize);
-          this.ctx.fillText(`${j}/${i}`, j*this.tileSize, i*this.tileSize+10);
-        }*/
         this.updateCacheTile(camera, j, i, map[i][j]);
       }
     }))
   }
 
-  /*updateTile(position:Vector, value: number){
-    const {x:tx, y:ty} = position;
-    if (this.map[ty][tx] != value){
-      this.map[ty][tx] = value;
-      const graphic = this.registred[value];
-      this.ctx.clearRect(tx*this.tileSize, ty*this.tileSize, this.tileSize, this.tileSize);
-      if (graphic){
-        this.ctx.drawImage(graphic, tx*this.tileSize, ty*this.tileSize, this.tileSize, this.tileSize);
-        this.ctx.fillText(`${tx}/${ty}`, tx*this.tileSize, ty*this.tileSize+10);
-      }
-    }
-  }*/
 }
